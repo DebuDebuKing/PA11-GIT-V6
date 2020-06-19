7a9e17909a358bd4381a7f7619e0591a65c65544
DebuDebuKing <48768830+DebuDebuKing@users.noreply.github.com>
2020-06-19 12:15:37 +0800

    Unity

---
+++ 16_Raushan_PA11/Assets/Scripts/ScoreUP.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUP : MonoBehaviour
+{
+  
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Spawnerscript.asd += 1;
+        }
+    }
+}
